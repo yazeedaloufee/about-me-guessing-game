ddd5cd3da27224fa6f5e0b8d8d1e1ce4ee5f1b67
YAZEED ALOUFEE <YAZEEDALOUFEE@GMAIL.COM>
2021-06-21 15:01:41 +0300

    added about me quiz

--- js/app.js
+++ js/app.js
@@ -1,60 +1,60 @@
-// 'use strict'  
-// let username= prompt("what is your name?")
-// alert("welcome "+username+". i will ask you a few questions about me if you dont mind.")
+'use strict'  
+let username= prompt("what is your name?")
+alert("welcome "+username+". i will ask you a few questions about me if you dont mind.")
 
-// let answer1 = prompt("am i an engineer? yes/no")
+let answer1 = prompt("am i an engineer? yes/no")
 
-// switch (answer1.toLocaleLowerCase()){
-//     case "yes":
-//     case "y" :
-//     alert("correct")
-//     break;
-//     default :
-//     alert("try harder")
-//     break;
-// }
+switch (answer1.toLocaleLowerCase()){
+    case "yes":
+    case "y" :
+    alert("correct")
+    break;
+    default :
+    alert("try harder")
+    break;
+}
 
-// let answer2 = prompt("do i have bachelors of sience in civil engineering? yes/no")
-// switch (answer2.toLocaleLowerCase()){
-//     case "yes":
-//     case "y" :
-//     alert("not really, i am an electronics engineer")
-//     break;
-//     default :
-//     alert("true, i am an electronics engineer")
-//     break;
-// }
+let answer2 = prompt("do i have bachelors of sience in civil engineering? yes/no")
+switch (answer2.toLocaleLowerCase()){
+    case "yes":
+    case "y" :
+    alert("not really, i am an electronics engineer")
+    break;
+    default :
+    alert("true, i am an electronics engineer")
+    break;
+}
 
-// let answer3 = prompt("have i worked at the german jordanian university? yes/no")
-// switch (answer3.toLocaleLowerCase()){
-//     case "yes":
-//     case "y" :
-//     alert("correct, I have worked there for 18 months")
-//     break;
-//     default :
-//     alert("you need to be more accurate, I have worked there for 18 months")
-//     break;
-// }
+let answer3 = prompt("have i worked at the german jordanian university? yes/no")
+switch (answer3.toLocaleLowerCase()){
+    case "yes":
+    case "y" :
+    alert("correct, I have worked there for 18 months")
+    break;
+    default :
+    alert("you need to be more accurate, I have worked there for 18 months")
+    break;
+}
 
-// let answer4 = prompt("have i worked as a teacher before? yes/no")
-// switch (answer4.toLocaleLowerCase()){
-//     case "yes":
-//     case "y" :
-//     alert("correct, but i don't want it anymore")
-//     break;
-//     default :
-//     alert("i have actully, but i don't want it anymore")
-//     break;
-// }
-// let answer5 = prompt("do i want to work at google? yes/no")
-// switch (answer5.toLocaleLowerCase()){
-//     case "yes":
-//     case "y" :
-//     alert("correct, pray for me")
-//     break;
-//     default :
-//     alert("incorrect. i really want that , pray form me!")
-//     break;
-// }
-// alert("so "+username+", try to read through the page to find more information about me. and refresh the page to answer the questions again. try to get them all right this time")
+let answer4 = prompt("have i worked as a teacher before? yes/no")
+switch (answer4.toLocaleLowerCase()){
+    case "yes":
+    case "y" :
+    alert("correct, but i don't want it anymore")
+    break;
+    default :
+    alert("i have actully, but i don't want it anymore")
+    break;
+}
+let answer5 = prompt("do i want to work at google? yes/no")
+switch (answer5.toLocaleLowerCase()){
+    case "yes":
+    case "y" :
+    alert("correct, pray for me")
+    break;
+    default :
+    alert("incorrect. i really want that , pray form me!")
+    break;
+}
+alert("so "+username+", try to read through the page to find more information about me. and refresh the page to answer the questions again. try to get them all right this time")
 
